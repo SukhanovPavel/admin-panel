e33a558e3acb3dbf34796e7b1ccd8f49595a8996
Pavel Sukhanov <psuhanov77@gmail.com>
2022-08-04 11:57:09 +0300

    add components

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './Checkbox.module.css';
+
+function App() {
+    return (
+        <div class="checkbox-pos border">
+            <input type="checkbox" class="check" name="" id=""></input>.
+            <input type="checkbox" class="check" checked="checked" name="" id=""></input>.
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Radio/Radio.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './Radio.module.css';
+
+function App() {
+    return (
+        <div class="radioPos">
+            <input type="radio" class="radio" name="" id=""></input>.
+            <input type="radio" class="radio" checked="checked" name="" id=""></input>.
+        </div>
+    )
+}
+
+export default App;
