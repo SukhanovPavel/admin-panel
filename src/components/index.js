6ff8baba702617b3d796c0828730c4a080479e9b
Pavel Sukhanov <psuhanov77@gmail.com>
2022-08-26 21:35:03 +0300

    create app

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button/Button.js";
+import Checkbox from "./Checkbox/Checkbox";
+import Dropdowns from "./Dropdowns/Dropdowns";
+import Input from "./Input/Input";
+import Radio from "./Radio/Radio";
+import Searchbar from "./Searchbar/Searchbar";
+
+export {
+    Button,
+    Checkbox,
+    Dropdowns,
+    Input,
+    Radio,
+    Searchbar,
+}
